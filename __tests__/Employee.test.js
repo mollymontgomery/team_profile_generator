b63f01211c568aa243621e93449877f8027fc506
Molly Montgomery <Molly@mollys-air-2.lan>
2021-12-11 15:15:38 -0600

    updated Employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,7 @@
+const Employee = require('../lib/Employee.js');
 const checkIfEqual = require('../lib/Employee.js');
 
-test ('checks if 10 is equal to 10', () => {
-    expect(checkIfEqual(10,10)).toBe(true);
-})
+test ('creates new employee object', () => {
+    const emplyee = new Employee();
+    expect(typeof(employee)).toBe('object');
+});
