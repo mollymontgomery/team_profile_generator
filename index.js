5eea0e4b2ca01b0ce4e851cfce23be2ccde7c509
Molly Montgomery <Molly@mollys-air-2.lan>
2021-12-12 16:59:45 -0600

    fixed index.js with all questions and writefile

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const fs = require('fs')
 const path = require('path')
 const inquirer = require('inquirer');
-const generatePage = require('./src/page-template');
-
+const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
+
+const team = [];
+const output = path.resolve()
+const outputPath = path.join(output, "team.html")
+
+const generatePage = require('./src/page-template');
 
 function addManager() {
-    return inquirer.prompt([
+    inquirer.prompt([
         {
             type: "input",
             name: "name",
@@ -26,30 +30,102 @@ function addManager() {
         },
         {
             type: "input",
-            name: "officeNumber",
+            name: "office",
             message: "What is the team manager's office number?",
         }
-    ])
+    ]).then(answers => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.office)
+        team.push(manager)
+        employeeQuestions()
+    })
+}
+
+function employeeQuestions() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Who do you want to add to the team?',
+            choices: ["Engineer", "Intern", "No more team members"],
+        }
+    ]).then(choice => {
+        if (choice.role === "Engineer") {
+            addEngineer();
+        } else if (choice.role === "Intern") {
+            addIntern();
+        } else {
+            console.log(team)
+            createTeam();
+        }
+    })
+}
+
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "Please enter your engineers's name:"
+        },
+        {
+            type: "input",
+            name: "engineerId",
+            message: "Enter the engineers's id:"
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is the engineer's email address?",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your enigneer's github username?",
+        }
+    ]).then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.enigineerEmail, answers.github)
+        team.push(engineer)
+        employeeQuestions()
+    })
+}
+
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "Please enter your intern's name:"
+        },
+        {
+            type: "input",
+            name: "internId",
+            message: "Enter the intern's id:"
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is the intern's email address?",
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is your intern's school?"
+        }
+    ]).then(answers => {
+        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.school)
+        team.push(intern)
+        employeeQuestions()
+    })
+}
+
+function createTeam(){
+    if(!fs.existsSync(output)){
+        fs.mkdirSync(output)
+    }
+    fs.writeFileSync(outputPath, render(team), "utf-8")
 }
 
 
-function addEngineer
- return inquirer.prompt([
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is the Engineers github username?',
-    },
-]);
-
-
-function addIntern
- return inquirer.prompt([
-    {
-        type: 'input',
-        name: 'school',
-        message: 'What school does the intern attend?',
-    },
-]);
+addManager()
 
 
--- src/page-template.js
+++ src/page-template.js
@@ -14,5 +14,32 @@ const teamManager = (Manager) => {
 }
 
 const teamEngineer = (Engineer) => {
-    
+    return `
+    <div class="card col" style="width: 250px">
+        <div class="card-header" style="width: 100%">
+        <h2>${Engineer.getName()}</h2>
+        </div>
+        <div class="card-body">
+        <p>ID: ${Engineer.getId()}</p>
+        <p>email: <a href="mailto:${Engineer.getEmail()}">${Manager.getEmail()}</a></p>
+        <p>Office: ${Engineer.getGithub()}</p>
+        </div>
+    </div>
+    `
+}
+
+
+const teamIntern = (Intern) => {
+    return `
+    <div class="card col" style="width: 250px">
+        <div class="card-header" style="width: 100%">
+        <h2>${Intern.getName()}</h2>
+        </div>
+        <div class="card-body">
+        <p>ID: ${Intern.getId()}</p>
+        <p>email: <a href="mailto:${Intern.getEmail()}">${Manager.getEmail()}</a></p>
+        <p>Office: ${Intern.getSchool()}</p>
+        </div>
+    </div>
+    `
 }
