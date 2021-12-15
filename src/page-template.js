afb9543d393d90a480962e9c7f78db91eb16bf12
Molly Montgomery <Molly@mollys-air-2.lan>
2021-12-14 21:14:41 -0600

    fixed error in getEmail in pagetemplate

--- src/page-template.js
+++ src/page-template.js
@@ -1,9 +1,9 @@
 const generateTeam = (team) => {
-console.log("Team from index.js", team) 
+    console.log("Team from index.js", team)
 
 
-const teamManager = (Manager) => {
-    return `
+    const teamManager = (Manager) => {
+        return `
     <div class="card col" style="width: 250px">
         <div class="card-header" style="width: 100%">
         <h2>${Manager.getRole()}</h2>
@@ -16,46 +16,47 @@ const teamManager = (Manager) => {
         </div>
     </div>
     `
-}
+    }
 
-const teamEngineer = (Engineer) => {
-    return `
+    const teamEngineer = (Engineer) => {
+        return `
     <div class="card col" style="width: 250px">
         <div class="card-header" style="width: 100%">
         <h2>${Engineer.getName()}</h2>
         </div>
         <div class="card-body">
         <p>ID: ${Engineer.getId()}</p>
-        <p>email: <a href="mailto:${Engineer.getEmail()}">${Manager.getEmail()}</a></p>
+        <p>email: <a href="mailto:${Engineer.getEmail()}">${Engineer.getEmail()}</a></p>
         <p>Office: ${Engineer.getGithub()}</p>
         </div>
     </div>
     `
-}
+    }
 
 
-const teamIntern = (Intern) => {
-    return `
+    const teamIntern = (Intern) => {
+        return `
     <div class="card col" style="width: 250px">
         <div class="card-header" style="width: 100%">
         <h2>${Intern.getName()}</h2>
         </div>
         <div class="card-body">
         <p>ID: ${Intern.getId()}</p>
-        <p>email: <a href="mailto:${Intern.getEmail()}">${Manager.getEmail()}</a></p>
+        <p>email: <a href="mailto:${Intern.getEmail()}">${Intern.getEmail()}</a></p>
         <p>Office: ${Intern.getSchool()}</p>
         </div>
     </div>
     `
 
-}
+    }
 
-const html = [];
+    const html = [];
 
     html.push(team.filter(employee => employee.getRole() === "Manager").map(manager => teamManager(manager)))
 
-   // html.push(team.filter(employee => employee.getRole() === "Engineer").map(engineer => teamEngineer(engineer)).join(""))
+    html.push(team.filter(employee => employee.getRole() === "Engineer").map(engineer => teamEngineer(engineer)).join(""))
 
+    html.push(team.filter(employee => employee.getRole() === "Intern").map(engineer => teamIntern(engineer)).join(""))
 
     return html.join("")
 
@@ -74,8 +75,13 @@ module.exports = team => {
     <title>Team Generator</title>
 </head>
 <body>
-    <div class='container'>
-    <div class='row'>
+    <header>
+        <nav class="navbar" id="navbar">
+            <span class="navbar-info mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+        </nav>
+    </header>
+    <main>
+        <div class='container'>
     ${generateTeam(team)}
     </div>
     </div>
